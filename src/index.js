61e2e7054db862d7226c20c6d1aceebe4c22c9ef
Natalia Barabina <barabash4ka@gmail.com>
2020-11-14 18:41:11 +0300

    add product

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import './images/bg/bg-main.jpg';
 // ./icon
 import './images/icon/favicon.svg';
 import './images/icon/icon-logo.svg';
+import './images/icon/icon-01.svg';
+import './images/icon/icon-02.svg';
+import './images/icon/icon-03.svg';
 
 // styles
 import './scss/fonts/index.scss';
