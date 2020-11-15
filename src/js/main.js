574c083c35ffd974e08031b34e16e2b0de9760df
Natalia Barabina <barabash4ka@gmail.com>
2020-11-15 15:41:50 +0300

    clear style

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,15 @@
-const burger = document.querySelector('.burger');
-const navbar  = document.querySelector('.nav-bar');
+const burger = document.querySelector('.nav__burger');
+const navbar  = document.querySelector('.nav__bar');
+const dropdown  = document.querySelector('.dropdown');
 
 burger.addEventListener('click', changeMenu)
+dropdown.addEventListener('click', changeDropdown)
 
 function changeMenu() {
    this.classList.toggle('is--close')
    navbar.classList.toggle('is--active')
+}
+
+function changeDropdown() {
+   this.classList.toggle('is--active')
 }
